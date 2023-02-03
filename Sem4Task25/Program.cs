8bf4ce225b90953bd0ffd3d1382bfb9b86f76a9e
Ekaterina <a9084235@yandex.ru>
2023-02-03 17:52:38 +0300

    add solution for task 25,27,29

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,59 @@
+﻿// №25 Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+ {
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+ }
+
+//  Метод вывода результата пользователю
+void PrintData(string msg1, int msg2)
+{
+    Console.WriteLine(msg1);
+    Console.WriteLine(msg2);
+}
+
+// Метод возводящий число А в натуральную степень B
+int Pow(int a, int b)
+{
+    int res = 1;
+    while (b>0)
+    {
+        b = b - 1;
+        res = res * a;
+    }
+    return res;
+ 
+}
+
+int numA = ReadData("введите число A:");
+int numB = ReadData("введите число B:");
+int result = Pow(numA,numB);
+PrintData("Натуральная степень: ", result);
+
+
+// Вариант 2
+// int numberA = ReadInt("Введите число A: ");
+// int numberB = ReadInt("Введите число B: ");
+// ToDegree(numberA, numberB);
+
+
+// // Функция возведения в степень
+// void ToDegree(int a, int b)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= b; i++)
+//     {
+//         result = result * a;
+//     }
+//     Console.WriteLine(result);
+// }
+
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//     Console.WriteLine(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,36 @@
+﻿// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в
+// числе.
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+ {
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+ }
+
+//  Метод вывода результата пользователю
+void PrintData(string msg1, int msg2)
+{
+    Console.WriteLine(msg1);
+    Console.WriteLine(msg2);
+}
+
+// Метод принимает на вход число и выдаёт сумму цифр в числе.
+int SumSym(int num)
+{
+    int res = 0;
+    while (num>0)
+    {
+        res = res + num%10;
+        num = num/10;
+    }
+    return res;
+ 
+}
+
+int numA = ReadData("введите число:");
+int result = SumSym(numA);
+PrintData("Сумма чисел: ", result);
+
+
+
