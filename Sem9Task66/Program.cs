896dfe0ffcd04a6ab385d527ee6b4d2e33ecf7da
Ekaterina <a9084235@yandex.ru>
2023-02-16 00:21:42 +0300

    add solution for task 63,64,65,66,67,68,69

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+  Console.Write(line);
+  int number = int.Parse(Console.ReadLine() ?? "0");
+  return number;
+}
+
+// Рекурсия считает сумму
+int RecSum(int m, int n)
+{
+  if (m >= n)
+  {
+    return n;
+  }
+  else
+  {
+    return m + RecSum(m + 1, n);
+  }
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+  Console.WriteLine(prefix);
+}
+
+int numM = ReadData("Введите число M: ");
+int numN = ReadData("Введите число N: ");
+int res = numM<numN? RecSum(numM,numN):RecSum(numN,numM);
+PrintResult(res);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+using System;
+using System.Numerics;
+
+// Чтение данных из консоли
+BigInteger ReadData(string line)
+{
+  Console.Write(line);
+  BigInteger number = BigInteger.Parse(Console.ReadLine() ?? "0");
+  return number;
+}
+
+// Рекуhсия Аккермана
+BigInteger Akkerman(BigInteger n, BigInteger m)
+{
+  if (n == 0)
+    return m + 1;
+  if (n != 0 && m == 0)
+    return Akkerman(n - 1, 1);
+  else
+    return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+BigInteger n = -1;
+BigInteger m = -1;
+while (n < 0 && m < 0)
+{
+//   Console.WriteLine("Введите два положительных числа: N и M.");
+  n = ReadData("Введите N: ");
+  m = ReadData("Введите M: ");
+}
+Console.WriteLine($"A  = {Akkerman(n, m)}");
